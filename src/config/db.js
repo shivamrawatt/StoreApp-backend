5975957b156882d714684f2729adfb121119d7c7
Shivam Rawat <shivamrawatt054@gmail.com>
2026-02-09 12:54:32 +0530

    db.js updated

--- src/config/db.js
+++ src/config/db.js
@@ -17,16 +17,17 @@
 
 
 
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'shopdb',
-  waitForConnections: true,
-  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 
 module.exports = pool;
 
--- temp.js
+++
@@ -1,6 +0,0 @@
-const db = require('./src/config/db');
-
-(async ()=>{
-  const [r] = await db.execute("SELECT 1 as ok");
-  console.log(r);
-})();
