536b8011d86bb3b0f37a986d2675e86ee20f4379
Shivam Rawat <tulyarthdigiweb68@gmail.com>
2026-08-01 17:21:30 +0530

    ci updated 1

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.js"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
